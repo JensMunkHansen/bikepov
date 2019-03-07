97def0eef29c4b87ff930de6aad0f5f406c4ad4f
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2019-03-07 21:25:19 +0100

    First commit

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+#include  <avr/io.h>
+#include <util/delay.h>  
+
+// Turns LED on and off
+int main (void)
+{
+  DDRB |= ((1 << DDB5));
+  while(1)
+  {
+    PORTB = ((0 << PB5));
+    _delay_ms (3000L);
+    PORTB = ((1 << PB5));
+    _delay_ms (3000L);
+  }
+  return 0;
+}
